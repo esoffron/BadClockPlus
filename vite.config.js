237051cc519def20ec1fb0bf746b06a5f45d5785
esoffron <4109731+esoffron@users.noreply.github.com>
2026-07-16 17:36:21 -0400

    Set up github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: '/BadClockPlus/',
   plugins: [viteSingleFile()],
   server: {
     port: 0,
